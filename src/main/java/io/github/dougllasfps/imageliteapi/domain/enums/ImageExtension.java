08a574937ef25278d449231ef256100cdb80ccb0
gabriel.vicente <gabrielvf64@gmail.com>
2024-06-21 09:10:05 -0300

    feat: save image through api

--- src/main/java/io/github/dougllasfps/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/domain/enums/ImageExtension.java
@@ -1,5 +1,24 @@
 package io.github.dougllasfps.imageliteapi.domain.enums;
 
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+
 public enum ImageExtension {
-    JPG, JPEG, PNG, GIF, BMP, SVG, WEBP
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG);
+
+    private final MediaType mediaType;
+
+    ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType) {
+        return Arrays.stream(values())
+                .filter(ie -> ie.mediaType.equals(mediaType))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid media type"));
+    }
 }
