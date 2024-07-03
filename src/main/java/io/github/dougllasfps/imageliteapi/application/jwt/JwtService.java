ac5725e2f2fe6ecd20488789af97216748943604
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-03 09:33:31 -0300

    feat: add generate token access

--- src/main/java/io/github/dougllasfps/imageliteapi/application/jwt/JwtService.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/application/jwt/JwtService.java
@@ -2,12 +2,47 @@ package io.github.dougllasfps.imageliteapi.application.jwt;
 
 import io.github.dougllasfps.imageliteapi.domain.AccessToken;
 import io.github.dougllasfps.imageliteapi.domain.entity.User;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
+    private SecretKeyGenerator secretKeyGenerator;
+
     public AccessToken generateAccessToken(User user) {
-        return new AccessToken("xxx");
+        SecretKey key = secretKeyGenerator.getKey();
+        Date expirationDate = generateExpirationDate();
+        Map<String, Object> claims = generateTokenClaims(user);
+
+        String token = Jwts.builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
+
+        return new AccessToken(token);
+    }
+
+    private Date generateExpirationDate() {
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName();
+        return claims;
     }
 }
